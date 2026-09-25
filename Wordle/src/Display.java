6936570a28ae2cacaa0d82364f08d539cc5ba361
carsten.ogburn <carsten.ogburn@MU-C1-16.mullen.pvt.k12.co.us>
2026-09-25 11:54:47 -0600

    Init Commit

---
+++ Wordle/src/Display.java
@@ -0,0 +1,15 @@
+
+public class Display
+	{
+
+
+		//Grid
+		static String [][] WordleGrid = new String[6][5];
+
+		
+		
+		public static void gridInit()
+		{
+			
+		}
+	}
---
+++ Wordle/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main
+	{
+
+		public static void main(String[] args)
+			{
+			
+				
+			}
+		
+		
+		
+		
+		public static String getWord()
+		{
+			int ranNumber = (int)(Math.random() * List.list.length) + 1;
+			return List.list[ranNumber];
+		}
+
+	}
