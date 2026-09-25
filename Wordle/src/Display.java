dc8f96489f1815ca7b41d1da1a86ddbab29cde40
carsten.ogburn <carsten.ogburn@MU-C1-16.mullen.pvt.k12.co.us>
2026-09-25 12:25:18 -0600

    Got the Display Init working

--- Wordle/src/Display.java
+++ Wordle/src/Display.java
@@ -10,6 +10,28 @@ public class Display
 		
 		public static void gridInit()
 		{
-			
+			for(int row = 0; row < WordleGrid.length; row ++ )
+			{
+				if(row == 0)
+				{
+					System.out.println(".___________________.");
+					System.out.println("|   |   |   |   |   |");
+					System.out.println("|---|---|---|---|---|");
+				}
+				else if(row == WordleGrid.length - 1)
+				{
+					System.out.println("|   |   |   |   |   |");
+					System.out.println("|___|___|___|___|___|");
+				}
+				else
+				{
+					System.out.println("|   |   |   |   |   |");
+					System.out.println("|---|---|---|---|---|");
+				}
+			}
 		}
 	}
+
+
+
+
